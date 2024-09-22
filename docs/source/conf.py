8fc8b9556076eae86a6a33fe18556e1a58cfc590
B0r4x <77775230+B0r4x@users.noreply.github.com>
2024-09-22 22:58:05 +0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,12 +1,12 @@
 # Configuration file for the Sphinx documentation builder.
-
+import sphinx_rtd_theme
 # -- Project information
 
 project = '0xC8'
 copyright = '2024, Alim'
 author = 'Alim'
 
-release = ''
+release = '0.1'
 version = ''
 
 # -- General configuration
@@ -17,6 +17,9 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.githubpages',
+    'recommonmark',
 ]
 
 intersphinx_mapping = {
@@ -33,3 +36,11 @@ html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+
+html_theme_options = {
+    'canonical_url': 'https://yourproject.readthedocs.io/',
+    'logo_only': True,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': True,
+}
